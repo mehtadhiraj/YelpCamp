d22479297ef779e0db47b38385aeaa43a54a0d7e
Mehta Dhiraj <mehtadhiraj21@gmail.com>
2018-07-07 22:08:52 +0000

    Authentication completed

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //Importing required packages
-var express      = require('express'),
-    app          = express(),
-    bodyParser   = require('body-parser'),
-    mongoose     = require("mongoose"),
-    Campground   = require("./models/Campgrounds"),
-    Comment      = require("./models/Comment"),
-    seedDB       = require("./seeds");
+var express               = require('express'),
+    app                   = express(),
+    bodyParser            = require('body-parser'),
+    mongoose              = require("mongoose"),
+    User                  = require("./models/User"),
+    Campground            = require("./models/Campgrounds"),
+    Comment               = require("./models/Comment"),
+    seedDB                = require("./seeds"),
+    passport              = require("passport"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
 
 //Connection to the database
 mongoose.connect("mongodb://localhost/yelp_camp");
@@ -17,6 +21,22 @@ app.use(express.static(__dirname + "/public"));
 //Calling a seed function from a sedds.js
 //seedDB();
 
+//Passport configuration
+app.use(require("express-session")({
+    secret: "YelpCamp is mine",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());;
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 //Call to a Landing page 
 app.get('/',function(req,res){
     res.render('landing');
@@ -34,13 +54,13 @@ app.get('/campgrounds',function(req,res){
     });
 });
 
-app.post("/campgrounds",function(req,res){
+app.post("/campgrounds", isLoggedIn, function(req,res){
     //Fetching place and image URL from textfield
     var place = req.body.place;
     var image = req.body.image;
     var description = req.body.description;
     //Creating a campground and adding to database.
-    Campground.create({place:place,image:image, description:description},function(error, campgrounds){
+    Campground.create({place:place,image:image, description:description}, function(error, campgrounds){
         if(error){
             console.log('There is an error\n'+error);
         }else {
@@ -66,7 +86,7 @@ app.get("/campgrounds/:id",function(req, res){
 //COMMENT ROUTES
 //==============================================
 //Route for a new comments
-app.get("/campgrounds/:id/comment/new",function(req, res){
+app.get("/campgrounds/:id/comment/new",isLoggedIn, function(req, res){
    Campground.findById(req.params.id,function(error, foundId){
        if(error){
            console.log("===============================\n"+error);
@@ -76,7 +96,7 @@ app.get("/campgrounds/:id/comment/new",function(req, res){
    })
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     var text = req.body.text;
     var name = req.body.name;
     Comment.create({text:text, name:name},function(error,comment){
@@ -89,7 +109,51 @@ app.post("/campgrounds/:id/comments", function(req, res){
        });
        
     });
-})
+});
+
+//===================
+//Auth Routes
+//===================
+app.get('/register', function(req, res) {
+    res.render('register');
+});
+
+app.post("/register", function(req, res) {
+    User.register({'username': req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register')
+        }else{
+            res.redirect('/campgrounds')
+        }
+    })
+});
+
+//Login form
+app.get('/login', function(req, res) {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}),function(){
+});
+
+//=====================
+//Logout route
+//=====================
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
 
 //Listening to port and IP addresses of the host
 app.listen(process.env.PORT,process.env.IP,function(req,res){
